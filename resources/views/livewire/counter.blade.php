9e5452aa41acf171142fe9896a76d58b322e1afc
Ruslan <rik.chanel@gmail.com>
2024-12-19 20:12:45 +0300

    some adaptation

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,16 +1,20 @@
 <div class="layout-content" id="{{ $name }}">
-    <h2 class="title title--2"> {{$title}}</h2>
-    <div class="counter-block">
-        @if(!empty($counters) )
-            @foreach($counters as $item)
-                <div class="counter-block__item">
-                    <div class="counter-block__item-number">
-                        <span class="counter" style="--from:0; --to:{{$item['number']}}; --time:4s;"></span>
-                        <span class="prefix">{{$item['prefix']}}</span>
+    <div class="counter-wrap">
+        <div class="counter-wrap-title">
+            <h2 class="title title--2"> {{$title}}</h2>
+        </div>
+        <div class="counter-block">
+            @if(!empty($counters) )
+                @foreach($counters as $item)
+                    <div class="counter-block__item">
+                        <div class="counter-block__item-number">
+                            <span class="counter" style="--from:0; --to:{{$item['number']}}; --time:4s;"></span>
+                            <span class="prefix">{{$item['prefix']}}</span>
+                        </div>
+                        <h3>{{$item['title']}}</h3>
                     </div>
-                    <h3>{{$item['title']}}</h3>
-                </div>
-            @endforeach
-        @endif
+                @endforeach
+            @endif
+        </div>
     </div>
 </div>
--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,6 +1,7 @@
 <header class="header">
     <div class="header-content">
 
+        <!-- Логотип -->
         <div class="header-logo" id="scroll-to-top">
             @if(!empty($logo['image_logo']))
                 <img src="{{$logo['image_logo']}}" alt="{{$logo['image_logo']}}">
@@ -8,11 +9,20 @@
                 {{$logo['text_logo']}}
             @endif
         </div>
-        <nav>
+
+        <!-- Кнопка бургер-меню -->
+        <button class="burger-menu" id="burger-menu-toggle" aria-label="Toggle navigation">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+
+        <!-- Навигация -->
+        <nav class="header-nav-wrapper">
             <ul class="header-nav">
                 @foreach($sections as $section)
                     @if($section['name'] !== \App\Enums\LandingEnum::SubHeader->name)
-                        <li>
+                        <li class="header-nav__item">
                             <a href="#{{ $section['name'] }}">
                                 {{ $section['short_label'] }}
                             </a>
@@ -25,7 +35,28 @@
 </header>
 
 <script>
+
     document.getElementById('scroll-to-top').addEventListener('click', function () {
-        window.scrollTo({top: 0, behavior: 'smooth'});
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+
+    const burgerMenuToggle = document.getElementById('burger-menu-toggle');
+    const navWrapper = document.querySelector('.header-nav-wrapper');
+
+    burgerMenuToggle.addEventListener('click', () => {
+        burgerMenuToggle.classList.toggle('active');
+        navWrapper.classList.toggle('open');
+    });
+
+
+    const navItems = document.querySelectorAll('.header-nav__item a');
+
+    navItems.forEach(item => {
+        item.addEventListener('click', () => {
+            burgerMenuToggle.classList.remove('active');
+            navWrapper.classList.remove('open');
+        });
     });
 </script>
+
